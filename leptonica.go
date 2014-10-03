3d14e95b0fdedd6f26916d47c4188ebcbe6f0eb1
AlasdairF <AlasdairF@users.noreply.github.com>
2014-10-03 16:27:58 +0700

    debug

--- leptonica.go
+++ leptonica.go
@@ -67,7 +67,7 @@ func NewPixReadMem(image *[]byte) (*goPix, error) {
 }
 
 // NewPixReadMem creates a new goPix instance from a byte array
-func (p *goPix) PixWriteMemPnm() (data []byte, err error) {
+func (p *goPix) PixWriteMemPnm() ([]byte, error) {
 	
 	var data []byte
 	ptr := C.uglycast(unsafe.Pointer(&(*data)[0]))
